289233d3a4ad2cb9c8facf92d9bfc86a1e03fd77
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2024-07-24 00:28:54 +0530

    actions script

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
+let recordFlag = false;
+
+let recorder;
 
 let constraints = {
     video: true,
     audio: true
 }
 
+// navigator -> global, browser info
 navigator.mediaDevices.getUserMedia(constraints)
 .then((stream) => {
     video.srcObject = stream;
+
+    recorder = new MediaRecorder(stream);
+})
+
+recordBtnCont.addEventListener("click", (e) => {
+    if (!recorder) return;
+
+    recordFlag = !recordFlag;
+
+    if (recordFlag) { //start
+        recorder.start();
+        recordBtn.classList.add("scale-record");
+    }
+    else { //stop
+        recorder.stop();
+        recordBtn.classList.remove("scale-record");
+    }
 })
