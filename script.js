46ceaf32b1de38dd4b16abeb5f7155be34b2f212
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2024-07-25 21:19:37 +0530

    double digit timer correction

--- script.js
+++ script.js
@@ -67,6 +67,10 @@ function startTimer() {
 
         let seconds = totalSeconds;
 
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
         timer.innerText = `${hours}:${minutes}:${seconds}`; 
         counter++;
     }
